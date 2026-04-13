736ed90179c098f53c5cc562898041974047c452
trevorgodfrey-eng <trevorgodfrey@majorleagueprofits.com>
2026-04-13 10:30:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ function isWithinSchedule() {
   const now = new Date();
   const day = now.getDay();
   const hour = now.getHours();
-  return day >= 1 && day <= 5 && hour >= 8 && hour < 20;
+  return day >= 1 && day <= 5 && hour >= 8 && hour < 19;
 }
 
 function getPeriodRange(period) {
